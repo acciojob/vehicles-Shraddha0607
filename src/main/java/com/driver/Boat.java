3d5c46fe94f86ef377cebb5a4ef46b92967426d8
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2024-02-17 19:41:23 +0530

    code added to complete the assignment

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,13 @@
+package com.driver;
+
+public class Boat {
+    private String name;
+    private int capacity;
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
